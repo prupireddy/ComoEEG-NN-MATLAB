82d11a3d83c33107c2797d0b416273807216b886
prupireddy <prupireddy@gmail.com>
2020-02-12 09:46:31 -0600

    Patient 6

--- NN_Train.m
+++ NN_Train.m
@@ -10,9 +10,9 @@
 % function properly.
 %% User-Defined Parameters
 
-in_str = 'P5_Features.mat'; % input filename
-mat_str = 'P5_NN.mat'; % output filename (.mat)
-onnx_str = 'P5_NN.onnx'; % output filename (ONNX network)
+in_str = 'P6_Features.mat'; % input filename
+mat_str = 'P6_NN.mat'; % output filename (.mat)
+onnx_str = 'P6_NN.onnx'; % output filename (ONNX network)
 
 train_rat = 0.5; % proportion of inputs to go into the training set
 
