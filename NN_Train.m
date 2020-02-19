e9aebd8ce6d836cf164e52502769593d2ffbcc87
prupireddy <prupireddy@gmail.com>
2020-02-19 16:11:16 -0600

    Added Comments

--- NN_Train.m
+++ NN_Train.m
@@ -1,9 +1,9 @@
 %% Metadata
 
-% This script takes as input a cell array and target vector output by
-% the script tk_deepprep. It randomly sorts this array into training and
-% test data and then trains an LSTM sequence classifier on it. The trained
-% neural network is both saved in a .mat file and exported to ONNX format
+% This script takes as input the Train and Testing sets from the Power
+% Spectral Density.It trains the LSTM classifier on the Training Set and on
+% the Testing set. It then converts the test set's predictions into a 3/5
+% analyzed prediction (post processed) and compares that with the Test Label. The neural network is both saved in a .mat file and exported to ONNX format
 % for later use in TensorRT.
 
 % MATLAB's Deep Learning Toolbox must be installed before this script will
@@ -19,8 +19,11 @@ load(in_str);
 %Restate n and m
 n = 3;
 m = 5;
+
+%Calculate the number of postprocessed predictions
 n_m_predictions = length(YTest);
 
+%Preallocate the PostProcessed predictions array
 YTruePred = zeros(n_m_predictions,1);
 
 numHiddenUnits = 200; % number of hidden units in the LSTM layer
@@ -41,7 +44,7 @@ options = trainingOptions('adam', ...
 % Load input variables
 load(in_str);
 
-% Categorize labels and display assignment results.
+% Categorize labels - puts this into 1s and 2s - meant for the classifier
 YTrain = categorical(YTrain);
 
 % Define LSTM Network Architecture
@@ -58,23 +61,32 @@ layers = [ ...
 % Train network
 net = trainNetwork(XTrain,YTrain,layers,options);
 
-% Test network accuracy using test set
+% Create Predictions on the Test Input
 YPred = classify(net,XTest);
 
+%Convert Predictions back into 0s and 1s for quantitative analysis
 YPred = double(YPred);
 YPred = YPred - ones(length(YPred));
+
+%Tracker for where we are in the n/m analysis 
 StartIndex = 1;
 
 for k = 1:n_m_predictions
+    %This calculates the ratio of positive and negative for a set of 5
+    %consecutive points in the Original Predictions
     predweight = mean(YPred(StartIndex:(StartIndex+(m-1)))); 
+    %If over the ratio, classify as positive
     if predweight >= (n/m)
         YTruePred(k)= 1;
     else
+    %If under the ratio, classify as negative
         YTruePred(k)= 0;
     end
+    %Reset the Start index for n/m analysis to the next set of 5 points
     StartIndex = StartIndex + m;
 end
 
+%Calculate the accuracy of the N/m analysis by comparing to the True Class
 acc = sum(YTruePred == YTest)./numel(YTest);
 disp(acc);
 
