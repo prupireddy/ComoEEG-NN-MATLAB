36b3262bf462985905cb659beaf4664679ccdcca
prupireddy <prupireddy@gmail.com>
2020-03-18 15:21:53 -0500

    Added Documentation

--- Boosting.m
+++ Boosting.m
@@ -1,16 +1,9 @@
 %% Explanation
 %This program takes in all of the PSD data as as input. It then computes
-%LDA scores for all of the data. Then, it calculates false positive and
-%true positive rates for all of the data so that the ROC curves could be
-%computed. The final output is an ROC curve.
-
-%Note: If the number of observations is less than the number of
-%features, You will get an error somewhere along the lines of - the matrix needs 
-%to be positive definite. You will need to perform PCA by uncommenting the
-%PCA code. 
-
-%In the future, this should also generate the x highest power interictals, where
-%x is the number of ictals. 
+%LDA scores for all of the data. Then, it generates the indices of 
+%ictals followed by the indices of x highest power interictals, where
+%x is the number of ictals. It also returns the number of ictals and high
+%power interictals, both of which are set to be the same. 
 %% Program
 
 %Import
@@ -24,18 +17,19 @@ load(input_str);
 proj=PSD_row*coeff(1,2).linear;
 
 %Isolation of ictals and high power interictals
-n_ictals = length(ictal_indices);
+n_ictals = length(ictal_indices); 
 n_high_power_interictals = n_ictals;
-proj_no_ictal = proj(interictal_indices);
-[sortedproj,unsortedIndices] = sort(proj_no_ictal);
-postextracted_high_power_indices = unsortedIndices(1:n_high_power_interictals);
-high_power_indices = interictal_indices(postextracted_high_power_indices);
-high_power_interictals = PSD_row(high_power_indices,:);
-ictals = PSD_row(ictal_indices,:);
+proj_no_ictal = proj(interictal_indices); %remove ictals
+[sortedproj,unsortedIndices] = sort(proj_no_ictal); %find the highest power interictals
+postextracted_high_power_indices = unsortedIndices(1:n_high_power_interictals); %find the original index in the interictal only matrix
+high_power_indices = interictal_indices(postextracted_high_power_indices); %find the original index in the matrix containing ictals
+high_power_interictals = PSD_row(high_power_indices,:); %high power interictials
+ictals = PSD_row(ictal_indices,:); %ictals
+%Create a new matrix that srores the ictals and high power interictals
 boostedPSD_row = zeros((n_ictals+n_high_power_interictals),176);
 boostedPSD_row((1:n_ictals),:)=ictals;
 boostedPSD_row(((n_ictals+1):(n_ictals+n_high_power_interictals)),:) = high_power_interictals;
-boostedIndices = vertcat(ictal_indices,high_power_indices);
+boostedIndices = vertcat(ictal_indices,high_power_indices); % create the output, the indices of ictal followed by high power interictal
 
 save(out_str,'boostedIndices','n_ictals','n_high_power_interictals','-v7.3');
 
