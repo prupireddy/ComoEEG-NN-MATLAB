fa06c1ac687d3634635fd924e5f4b7e64cec993a
prupireddy <prupireddy@gmail.com>
2020-03-18 15:21:10 -0500

    Added documentation

--- Series.m
+++ Series.m
@@ -1,6 +1,25 @@
+%% Overview
+
+% This takes in as an input the locations of the high power interictals, number of ictals and interictals (high power) and outputs the
+% full power spectral density, 2640 features, to be inputted into the LSTM NN. 
+% This was necessary because the LSTM is used to taking in as an input a series, and this turns
+% the 176 feature PSD into a series. 
+% 
+% This program works like the normal power spectral density, just with the exception that instead of targetting
+% all of the 20s segments, it targets the 20s segments where there is boosted data. It does this with a 
+% little equation that converts the index retrieved from the Boosting program to the precise time steps 
+% of the 20s window. It iterates over all indices. 
+% 
+% As an output, it generates the 2640 PSD as well as a state array that tells whether each PSD is ictal 
+% or interictal. The first half of all the states are ictal and the second
+% half are interictal. Therefore, the both are arrays with dimensions of
+% number ictal plus number of high power interictal by 1. Each cell in the
+% cell array for inputs has 2640 features and each number in the state
+% array is 0 (second half) or 1 (first half). 
+
 %% Program
 data_str = 'P10_EEG.mat'; % EEG input filename
-boostedDataInfo = 'P10_BoostedDataInfo';
+boostedDataInfo = 'P10_BoostedDataInfo'; %Indices of ictal or interictal
 %out_str = 'P10_FullPSD_176.mat'; % output filename
 out_str = 'P10_BoostedPSD_2640.mat';
 % Input and output filenames. Use full path names or move MATLAB's working
@@ -46,7 +65,7 @@ sp_temp = spectrogram(d_temp,window,noverlap,nfft,srate);
 [M,N] = size(sp_temp);
 
 %Here we calculate the number of trials 
-n_tr = n_high_power_interictals +n_ictals;
+n_tr = n_high_power_interictals +n_ictals;%These two come from the  boosted data information
 %Stores PSD - this is in Tyler's original format
 nn_inputs = cell(n_tr,1);
 %This corresponds to each PSD 1-1: it is the label matrix
