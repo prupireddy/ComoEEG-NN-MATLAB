bc8ca10f967de151873a580ba09e464db048b2aa
prupireddy <prupireddy@gmail.com>
2020-04-07 15:09:40 -0500

    Boosting and Spectrogram Generation of the new at least one ictal requirement

--- Boosting.m
+++ Boosting.m
@@ -11,7 +11,8 @@
 
 %Import
 data_str = 'P10_EEG.mat';
-input_str = 'P10_TFullPSD_176.mat';
+%input_str = 'P10_TFullPSD_176.mat';
+input_str = 'P10_TIFullPSD_176.mat';
 load(data_str);
 load(input_str);
 
