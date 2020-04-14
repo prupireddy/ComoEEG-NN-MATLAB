69dad3a7933a1d80db2a14e0b1b6a6d63b840ee6
prupireddy <prupireddy@gmail.com>
2020-04-14 11:00:17 -0500

    Rough Draft of find min and find max changes

--- Boosting.m
+++ Boosting.m
@@ -53,57 +53,94 @@ boostedPSD_row(((n_ictals+1):(n_ictals+n_high_power_interictals)),:) = high_powe
 %Create the State array that corresponds to each of the PSD index-by-index
 boostedStateArray = zeros((n_ictals+n_high_power_interictals),1);
 boostedStateArray(1:n_ictals) = 1;
-    
-mkdir ictal %ictal folder
-fpath = strcat(pwd,'\ictal'); %path to the folder
-baseStr = erase(data_str,"EEG.mat");
-baseStr = strcat(fpath,'\',baseStr);%Used as the base for the file name
-for l = 1:n_ictals %iterate over each ictal observation
-    fileStr = strcat(baseStr,num2str(l),'.TIFF'); %Complete the name
-    i_psd = boostedIndices(l);%locate the index of the observation in interest
-    start = 1 + (tr_pts)*(i_psd-1); %Calculate the start and end of the points
-    stop = start + (tr_pts - 1);
-    for c = 1:n_chan
-        S=spectrogram(diff(filtfilt(d,data(c,start:stop))),512,256); 
-        h = imagesc(log(abs(S))); %image handle
-        colormap('gray')
-        H = getimage(h); %Image Data
-        H = (H - min(H,[],'all'))/(max(H,[],'all')-min(H,[],'all')); %0-1: required for TIFF
-        %H = imresize(H,[875 656]);
-        if c == 1
-            imwrite(H,fileStr); %Form and Save Base layer of TIFF
-        else
-            imwrite(H,fileStr,'WriteMode','append'); %Stack and save subsequent layers
+
+globalMax = 0
+globalMin = 200
+
+for l = 1:n_ictals
+    i_psd = boostedIndices(l)
+    start = 1 + (tr_pts)*(i_psd - 1)
+    stop  = start + (tr_pts - 1)
+    for c = 1:chan
+        S = spectrogram(diff(filtfilt(d,data(c,start:stop))),512,256);
+        currentMax = max(abs(S),[],'all')
+        if currentMax > globalMax
+            globalMax = currentMax
+        end
+        currentMin = min(abs(S),[],'all')
+        if currentMin < globalMin 
+            globalMin = currentMin
         end
     end
-end   
-
+   
 n_tr = n_ictals + n_high_power_interictals;
-mkdir interictal
-fpath = strcat(pwd,'\interictal');
-baseStr = erase(data_str,"EEG.mat");
-baseStr = strcat(fpath,'\',baseStr);
 for l = (n_ictals+1):n_tr
-    fileStr = strcat(baseStr,num2str(l),'.TIFF');
     i_psd = boostedIndices(l);
     start = 1 + (tr_pts)*(i_psd-1);
     stop = start + (tr_pts - 1);
     for c = 1:n_chan
         S=spectrogram(diff(filtfilt(d,data(c,start:stop))),512,256);
-        h = imagesc(log(abs(S))); %image handle
-        colormap('gray')
-        H = getimage(h);
-        H = (H - min(H,[],'all'))/(max(H,[],'all')-min(H,[],'all'));
-        %H = imresize(H,[875 656]);
-        if c == 1
-            imwrite(H,fileStr);
-        else
-            imwrite(H,fileStr,'WriteMode','append');
+        currentMax = max(abs(S),[],'all')
+        if currentMax > globalMax
+            globalMax = currentMax
+        end
+        currentMin = min(abs(S),[],'all')
+        if currentMin < globalMin 
+            globalMin = currentMin
         end
     end
-end
-
-
-
-
-
+     
+    
+% mkdir ictal %ictal folder
+% fpath = strcat(pwd,'\ictal'); %path to the folder
+% baseStr = erase(data_str,"EEG.mat");
+% baseStr = strcat(fpath,'\',baseStr);%Used as the base for the file name
+% for l = 1:n_ictals %iterate over each ictal observation
+%     fileStr = strcat(baseStr,num2str(l),'.TIFF'); %Complete the name
+%     i_psd = boostedIndices(l);%locate the index of the observation in interest
+%     start = 1 + (tr_pts)*(i_psd-1); %Calculate the start and end of the points
+%     stop = start + (tr_pts - 1);
+%     for c = 1:n_chan
+%         S=spectrogram(diff(filtfilt(d,data(c,start:stop))),512,256); 
+%         h = imagesc(log(abs(S))); %image handle
+%         colormap('gray')
+%         H = getimage(h); %Image Data
+%         H = (H - min(H,[],'all'))/(max(H,[],'all')-min(H,[],'all')); %0-1: required for TIFF
+%         %H = imresize(H,[875 656]);
+%         if c == 1
+%             imwrite(H,fileStr); %Form and Save Base layer of TIFF
+%         else
+%             imwrite(H,fileStr,'WriteMode','append'); %Stack and save subsequent layers
+%         end
+%     end
+% end   
+% 
+% n_tr = n_ictals + n_high_power_interictals;
+% mkdir interictal
+% fpath = strcat(pwd,'\interictal');
+% baseStr = erase(data_str,"EEG.mat");
+% baseStr = strcat(fpath,'\',baseStr);
+% for l = (n_ictals+1):n_tr
+%     fileStr = strcat(baseStr,num2str(l),'.TIFF');
+%     i_psd = boostedIndices(l);
+%     start = 1 + (tr_pts)*(i_psd-1);
+%     stop = start + (tr_pts - 1);
+%     for c = 1:n_chan
+%         S=spectrogram(diff(filtfilt(d,data(c,start:stop))),512,256);
+%         h = imagesc(log(abs(S))); %image handle
+%         colormap('gray')
+%         H = getimage(h);
+%         H = (H - min(H,[],'all'))/(max(H,[],'all')-min(H,[],'all'));
+%         %H = imresize(H,[875 656]);
+%         if c == 1
+%             imwrite(H,fileStr);
+%         else
+%             imwrite(H,fileStr,'WriteMode','append');
+%         end
+%     end
+% end
+% 
+% 
+% 
+% 
+% 
