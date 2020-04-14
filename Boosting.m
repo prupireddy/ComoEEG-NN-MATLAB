f877de1f8910ad9ce3b1bcc6bdea02ca0a1c9c95
prupireddy <prupireddy@gmail.com>
2020-04-14 16:26:26 -0500

    In progress - functioning multitaper working on 60 Hz

--- Boosting.m
+++ Boosting.m
@@ -19,9 +19,10 @@
 %Import
 data_str = 'P10_EEG.mat';
 
-d = designfilt('bandstopiir','FilterOrder',2, ...
-               'HalfPowerFrequency1',59,'HalfPowerFrequency2',61, ...
-               'DesignMethod','butter','SampleRate',256);
+%Filter:
+% d = designfilt('bandstopiir','FilterOrder',2, ...
+%                'HalfPowerFrequency1',59,'HalfPowerFrequency2',61, ...
+%                'DesignMethod','butter','SampleRate',256);
            
 %input_str = 'P10_TFullPSD_176.mat';
 %input_str = 'P10_TIFullPSD_176.mat';
@@ -57,40 +58,54 @@ boostedStateArray(1:n_ictals) = 1;
 globalMax = 0;
 globalMin = 200;
 
+movingwin = [2 1];
+params.tapers = [4 7];
+params.pad = 0;
+params.Fs = 256;
+params.fpass = [0 128];
+params.err = 0;
+params.trialave = 0;
+
 for l = 1:n_ictals
-    i_psd = boostedIndices(l);
+    i_psd = boostedIndices(1);
     start = 1 + (tr_pts)*(i_psd - 1);
     stop  = start + (tr_pts - 1);
-    for c = 1:chan
-        S = spectrogram(diff(filtfilt(d,data(c,start:stop))),512,256);
-        currentMax = max(abs(S),[],'all');
-        if currentMax > globalMax
-            globalMax = currentMax;
-        end
-        currentMin = min(abs(S),[],'all');
-        if currentMin < globalMin 
-            globalMin = currentMin;
-        end
+    for c = 1:n_chan
+       %[S,t,f] = mtspecgramc(diff(filtfilt(d,data(15,start:stop))),movingwin,params);filtered
+       [S,t,f] = mtspecgramc(diff(data(15,start:stop)),movingwin,params);
+       S = S';
+       S = log(abs(S));
+       S = mean(S,2);
+       f = f';
+       plot(f,S)
+       currentMax = max((S),[],'all');
+       if currentMax > globalMax
+           globalMax = currentMax;
+       end
+       currentMin = min((S),[],'all');
+       if currentMin < globalMin 
+           globalMin = currentMin;
+       end
     end
 end
 
-n_tr = n_ictals + n_high_power_interictals;
-for l = (n_ictals+1):n_tr
-    i_psd = boostedIndices(l);
-    start = 1 + (tr_pts)*(i_psd-1);
-    stop = start + (tr_pts - 1);
-    for c = 1:n_chan
-        S=spectrogram(diff(filtfilt(d,data(c,start:stop))),512,256);
-        currentMax = max(abs(S),[],'all');
-        if currentMax > globalMax
-            globalMax = currentMax;
-        end
-        currentMin = min(abs(S),[],'all');
-        if currentMin < globalMin 
-            globalMin = currentMin;
-        end
-    end
-end
+% n_tr = n_ictals + n_high_power_interictals;
+% for l = (n_ictals+1):n_tr
+%     i_psd = boostedIndices(l);
+%     start = 1 + (tr_pts)*(i_psd-1);
+%     stop = start + (tr_pts - 1);
+%     for c = 1:n_chan
+%         S=spectrogram(diff(filtfilt(d,data(c,start:stop))),512,256);
+%         currentMax = max(log(abs(S)),[],'all');
+%         if currentMax > globalMax
+%             globalMax = currentMax;
+%         end
+%         currentMin = min(log(abs(S)),[],'all');
+%         if currentMin < globalMin 
+%             globalMin = currentMin;
+%         end
+%     end
+% end
     
 % mkdir ictal %ictal folder
 % fpath = strcat(pwd,'\ictal'); %path to the folder
