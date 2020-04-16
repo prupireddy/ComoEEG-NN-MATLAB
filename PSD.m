bd2988bf1a651b047cefe799703fd952a2658d7a
prupireddy <prupireddy@gmail.com>
2020-04-16 12:26:53 -0500

    Changed patient number

--- PSD.m
+++ PSD.m
@@ -39,12 +39,12 @@
 %original output, but it is not in the final output. 
 %% User-Defined Parameters
 
-data_str = 'P5_EEG.mat'; % input filename (data)
-times_str = 'P5_Annotations.xlsx'; % input filename (seizure times)
+data_str = 'P6_EEG.mat'; % input filename (data)
+times_str = 'P6_Annotations.xlsx'; % input filename (seizure times)
 %out_str = 'P10_TFullPSD_176.mat'; % output filename
 %out_str = 'P10_FullPSD.mat';
 %out_str = 'P10_TIFullPSD_176.mat'
-out_str = 'P5_TNIFullPSD_176.mat'
+out_str = 'P6_TNIFullPSD_176.mat'
 % Input and output filenames. Use full path names or move MATLAB's working
 % directory to the correct location beforehand. Output extension should be
 % .mat.
