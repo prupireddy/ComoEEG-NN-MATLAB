9f8812e8c1818cb493396c2d07480e23618b0cc7
prupireddy <prupireddy@gmail.com>
2020-05-05 18:59:27 -0500

    Changed patient number

--- EDF_to_MFile.m
+++ EDF_to_MFile.m
@@ -11,8 +11,8 @@
 
 %% User-Defined Parameters  
 
-edf_name = 'Patient #8.edf';
-mat_name = 'P8_DEEG.mat';
+edf_name = 'Patient #10.edf';
+mat_name = 'P10_DEEG.mat';
 % Input and output filenames. Use full path names or move MATLAB's working
 % directory to the correct location beforehand. Output extension should be
 % .mat.
